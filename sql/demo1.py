a71eb31777aa1b93a517ecbc4a4108a03ba98ed1
御坂美琴 <lzlloveyou@vip.qq.com>
2023-07-23 22:41:50 +0800

    python简单控制mysql

--- sql/demo1.py
+++ sql/demo1.py
@@ -5,8 +5,21 @@ conn = Connection(
     port=3306,
     user='root',
     password='',
+    autocommit=True,
 )
 # 打印mysql信息
-print(conn.get_server_info())
+# print(conn.get_server_info())
+
+curses = conn.cursor()
+conn.select_db("xz")
+
+curses.execute("insert into xz_user values(5, 'ak47', '123456', 'ming@qq.com', '13501234560', 'img/avatar/default.png', 'gun', 1)")
+# #手动确认
+# conn.commit()
+
+curses.execute("select * from xz_user")
+res = curses.fetchall()
+for r in res:
+    print(r)
 # 关闭链接
 conn.close()
